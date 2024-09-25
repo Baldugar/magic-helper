d87e3fb76d73c74f70e4e82c7b9e646a2a61c4c9
Alejandro López <alejandro.lopez@qubidu.com>
2024-09-25 22:11:38 +0200

    remove json files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,49 +1,43 @@
 import { Container, Typography } from '@mui/material'
 import { useEffect } from 'react'
 import './App.css'
-import jsonCards from './assets/cards.json'
-import yugiohCards from './assets/yugioh_cards.json'
-import yugiohCardsGame from './assets/yugioh_cards_game.json'
 import { MTGAFunctions } from './graphql/MTGA/functions'
-import CardsContext from './hooks/cardsContext'
-import { MTGACard } from './types/types'
 
-type YCard = {
-    name: string
-}
+// type YCard = {
+//     name: string
+// }
 
 function App() {
     const {
         queries: { getMTGACards },
     } = MTGAFunctions
-    const cards: MTGACard[] = jsonCards as MTGACard[]
-    const yCards = (yugiohCards as { data: Array<YCard> }).data
-    const yCardsGame1 = (yugiohCardsGame as { data: Array<YCard> }).data
+    // const yCards = (yugiohCards as { data: Array<YCard> }).data
+    // const yCardsGame1 = (yugiohCardsGame as { data: Array<YCard> }).data
 
-    const definiteCards: Array<YCard> = []
-    for (const card of yCards) {
-        const cGameIdx = yCardsGame1.findIndex((c) => c.name === card.name)
-        if (cGameIdx !== -1) {
-            const cGame = yCardsGame1[cGameIdx]
-            definiteCards.push(cGame)
-            yCardsGame1.splice(cGameIdx, 1)
-        }
-    }
+    // const definiteCards: Array<YCard> = []
+    // for (const card of yCards) {
+    //     const cGameIdx = yCardsGame1.findIndex((c) => c.name === card.name)
+    //     if (cGameIdx !== -1) {
+    //         const cGame = yCardsGame1[cGameIdx]
+    //         definiteCards.push(cGame)
+    //         yCardsGame1.splice(cGameIdx, 1)
+    //     }
+    // }
 
-    const yCardsGame2 = (yugiohCardsGame as { data: Array<YCard> }).data
-    const missingCards: Array<YCard> = []
-    for (const card of yCardsGame2) {
-        const cGameIdx = yCards.findIndex((c) => c.name === card.name)
-        if (cGameIdx === -1) {
-            missingCards.push(card)
-        }
-    }
+    // const yCardsGame2 = (yugiohCardsGame as { data: Array<YCard> }).data
+    // const missingCards: Array<YCard> = []
+    // for (const card of yCardsGame2) {
+    //     const cGameIdx = yCards.findIndex((c) => c.name === card.name)
+    //     if (cGameIdx === -1) {
+    //         missingCards.push(card)
+    //     }
+    // }
 
-    console.log('definiteCards', definiteCards)
-    console.log(
-        'missingCards',
-        missingCards.map((c) => c.name),
-    )
+    // console.log('definiteCards', definiteCards)
+    // console.log(
+    //     'missingCards',
+    //     missingCards.map((c) => c.name),
+    // )
 
     useEffect(() => {
         getMTGACards().then((cards) => {
@@ -54,18 +48,30 @@ function App() {
     //    console.log(cards.filter((card) => card.card_faces && card.card_faces.filter((cf) => !cf.oracle_text).length > 0))
 
     return (
-        <CardsContext.Provider value={cards}>
-            <Container maxWidth={'sm'}>
-                <Typography variant={'h1'}>Hello World</Typography>
-                {/* <SelectColor />
+        <Container maxWidth={'sm'}>
+            <Typography variant={'h1'}>Hello World</Typography>
+            {/* <SelectColor />
                 <SelectRarity />
                 <SelectType />
                 <Grid xs={12} container>
                     <RandomCommanderSelector />
                 </Grid> */}
-            </Container>
-        </CardsContext.Provider>
+        </Container>
     )
+
+    // return (
+    //     <CardsContext.Provider value={cards}>
+    //         <Container maxWidth={'sm'}>
+    //             <Typography variant={'h1'}>Hello World</Typography>
+    //             {/* <SelectColor />
+    //             <SelectRarity />
+    //             <SelectType />
+    //             <Grid xs={12} container>
+    //                 <RandomCommanderSelector />
+    //             </Grid> */}
+    //         </Container>
+    //     </CardsContext.Provider>
+    // )
 }
 
 export default App
