d8fba50a27117ade7d78194139999f47c10600a8
Baldugar <alejandro.lopez.alegre@gmail.com>
2025-02-26 23:25:51 +0100

    we now have a 3d deckbox (#19)

--- client/src/utils/functions/deckFunctions.ts
+++ client/src/utils/functions/deckFunctions.ts
@@ -21,3 +21,24 @@ export const calculateNewDeck = (
     const newZones = calculateZonesFromNodes(nodes)
     setDeck({ ...deck, cards: [...newCards], zones: newZones })
 }
+
+/**
+ * Generates an array of unique random integers from 0 to maxNumber (inclusive).
+ *
+ * @param maxNumber - The maximum number (inclusive) in the range.
+ * @param occurrences - The number of unique integers to return.
+ * @returns An array of unique integers. If occurrences exceeds maxNumber + 1, returns all available numbers.
+ */
+export const getUniqueRandomIntegers = (maxNumber: number, occurrences: number): number[] => {
+    // Create an array with all numbers from 0 to maxNumber.
+    const numbers: number[] = Array.from({ length: maxNumber + 1 }, (_, i) => i)
+
+    // Shuffle the array using Fisher-Yates shuffle.
+    for (let i = numbers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        ;[numbers[i], numbers[j]] = [numbers[j], numbers[i]]
+    }
+
+    // Return as many numbers as available, up to the requested occurrences.
+    return numbers.slice(0, Math.min(occurrences, numbers.length))
+}
